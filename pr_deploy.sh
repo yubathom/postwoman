34264e0837d15b43604bfe7ee11d4d26f0a3d7f5
yubathom <yubathom@gmail.com>
2019-09-11 17:55:43 -0300

    Syntax

--- pr_deploy.sh
+++ pr_deploy.sh
@@ -3,13 +3,12 @@ if [ “$TRAVIS_PULL_REQUEST” == “false” ]; then
 echo “Not a PR. Skipping surge deployment.”
 exit 0
 fi
-npm run build
 npm i -g surge
 
 export SURGE_LOGIN=${SURGE_LOGIN}
 export SURGE_TOKEN=${SURGE_TOKEN}
 export DEPLOY_DOMAIN=https://pr-${TRAVIS_PULL_REQUEST}-postwoman.surge.sh
 
-surge —project ./dist —domain $DEPLOY_DOMAIN;
+surge -project ./dist -domain $DEPLOY_DOMAIN;
 
 echo $DEPLOY_DOMAIN;
