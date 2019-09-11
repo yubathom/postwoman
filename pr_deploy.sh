ef0f3fed6bdd9be51a5881c0945090f06901d422
yubathom <yubathom@gmail.com>
2019-09-11 17:02:57 -0300

    PR's auto deployiments with surge.sh

---
+++ pr_deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if [ “$TRAVIS_PULL_REQUEST” == “false” ]; then
+echo “Not a PR. Skipping surge deployment.”
+exit 0
+fi
+npm i
+npm run build
+npm i -g surge
+
+export SURGE_LOGIN=${SURGE_LOGIN}
+export SURGE_TOKEN=${SURGE_TOKEN}
+export DEPLOY_DOMAIN=https://pr-${TRAVIS_PULL_REQUEST}-postwoman.surge.sh
+
+surge -—project ./dist -—domain $DEPLOY_DOMAIN;
