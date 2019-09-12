e2b01042d7501a40d46981f182d27c36a5609af6
yubathom <yubathom@gmail.com>
2019-09-12 20:45:44 -0300

    testing

--- pr_deploy.sh
+++ pr_deploy.sh
@@ -11,20 +11,19 @@ DEPLOY_DOMAIN=https://postwoman-preview-pr-${TRAVIS_PULL_REQUEST}.surge.sh
 
 surge --project ./dist --domain $DEPLOY_DOMAIN;
 
-curl
-  --silent
-  --output /dev/null
-  -H "Authorization: token ${GITHUB_ACCESS_TOKEN}"
-  -X POST \ 
+curl  -X POST \
+  --silent \
+  --output /dev/null \
+  -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
   -d "{\"body\": \"${DEPLOY_DOMAIN}\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 
 CYPRESS_RUN_RESULT=$(CYPRESS_baseUrl="${DEPLOY_DOMAIN}" npx cypress run)
 CYPRESS_RUN_RESULT=${CYPRESS_RUN_RESULT%$'\r'}
 
-curl
-  --silent 
-  --output /dev/null 
-  -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X POST \
+curl -X POST \
+  --silent \
+  --output /dev/null \
+  -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
   -d -e '{"body":"'"$CYPRESS_RUN_RESULT"'"}' "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 
 echo -e $CYPRESS_RUN_RESULT
