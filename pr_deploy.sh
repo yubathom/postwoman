764307d9e0510187a5ae7d76a9241b26e9359e1e
yubathom <yubathom@gmail.com>
2019-09-12 15:57:48 -0300

    Testing cypress test at deployed PR at surge url

--- pr_deploy.sh
+++ pr_deploy.sh
@@ -11,6 +11,9 @@ export DEPLOY_DOMAIN=https://postwoman-pr-${TRAVIS_PULL_REQUEST}.surge.sh
 
 surge --project ./dist --domain $DEPLOY_DOMAIN;
 
+CYPRESS_baseUrl="https://postwoman.pull-request-${TRAVIS_PULL_REQUEST}.surge.sh" npx cypress run
+
 curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X POST \
--d "{\"body\": \"https://postwoman-pr-${TRAVIS_PULL_REQUEST}.surge.sh\"}" \
+-d "{\"body\": \"https://postwoman.pull-request-${TRAVIS_PULL_REQUEST}.surge.sh\"}" \
 "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+
