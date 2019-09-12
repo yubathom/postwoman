838316f22b4cde296955b356161ce2fec954058d
yubathom <yubathom@gmail.com>
2019-09-12 16:22:02 -0300

    Test

--- pr_deploy.sh
+++ pr_deploy.sh
@@ -4,16 +4,17 @@ echo “Not a PR. Skipping surge deployment.”
 exit 0
 fi
 npm i -g surge
+npm i -g cypress
 
 export SURGE_LOGIN=${SURGE_LOGIN}
 export SURGE_TOKEN=${SURGE_TOKEN}
-export DEPLOY_DOMAIN=https://postwoman-pr-${TRAVIS_PULL_REQUEST}.surge.sh
+export DEPLOY_DOMAIN=https://postwoman-preview-pr-${TRAVIS_PULL_REQUEST}.surge.sh
 
 surge --project ./dist --domain $DEPLOY_DOMAIN;
 
-CYPRESS_baseUrl="https://postwoman.pull-request-${TRAVIS_PULL_REQUEST}.surge.sh" npx cypress run
+CYPRESS_baseUrl="${DEPLOY_DOMAIN}" cypress run
 
-curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X POST \
--d "{\"body\": \"https://postwoman.pull-request-${TRAVIS_PULL_REQUEST}.surge.sh\"}" \
+curl  --silent --output /dev/null -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X POST \
+-d "{\"body\": \"${DEPLOY_DOMAIN}\"}" \
 "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 
