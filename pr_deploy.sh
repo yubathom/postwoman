ac70c66b6dfc058fed56a636f70417eb1adb23de
yubathom <yubathom@gmail.com>
2019-09-12 13:47:47 -0300

    Add comment via curl at PRs

--- pr_deploy.sh
+++ pr_deploy.sh
@@ -7,8 +7,10 @@ npm i -g surge
 
 export SURGE_LOGIN=${SURGE_LOGIN}
 export SURGE_TOKEN=${SURGE_TOKEN}
-export DEPLOY_DOMAIN=https://pr-${TRAVIS_PULL_REQUEST}-postwoman.surge.sh
+export DEPLOY_DOMAIN=https://postwoman-pr-${TRAVIS_PULL_REQUEST}.surge.sh
 
 surge --project ./dist --domain $DEPLOY_DOMAIN;
 
-echo $DEPLOY_DOMAIN;
+curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X POST \
+-d "{\"body\": \"Pull request preview deployed at:\\n${$DEPLOY_DOMAIN}\"}"\
+"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
