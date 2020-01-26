ec5d19ab06d91b6cd2652c3d4b05da21705a6f98
yubathom <yubathom@gmail.com>
2020-01-26 19:21:20 -0300

    test: create lib dev command for lib testing

---
+++ lib/dev.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { Logo } from './lib-components/index';
+
+export default Vue.extend({
+  name: 'DevLibrary',
+  components: {
+    Logo
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+     <logo :color="'rgba(80, 250, 123, 1)'" />
+  </div>
+</template>
