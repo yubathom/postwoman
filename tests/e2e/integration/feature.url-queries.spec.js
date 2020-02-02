36efde50c17de9a5712bbfd77848b59cb0254ace
yubathom <yubathom@gmail.com>
2020-02-02 01:31:38 -0300

    tests: update queries spec (disable auth info)

--- tests/e2e/integration/feature.url-queries.spec.js
+++ tests/e2e/integration/feature.url-queries.spec.js
@@ -1,46 +1,34 @@
-describe('Methods', () => {
-  const methods = [ 'POST', 'PUT']
-  methods.forEach((method) => {
-      it(`Change the default method GET to ${method} with url query`, () => {
-        cy.visit(`/?method=${method}`)
-          .get('#method').should('have.value', method)
+describe('Authentication', () => {
+  it(`Change default auth user and pass with url`, () => {
+    cy.visit(`?&auth=Basic Auth&httpUser=foo&httpPassword=bar`, { retryOnStatusCodeFailure: true })
+      .get('input[name="http_basic_user"]', { timeout: 500 })
+      .invoke('val')
+      .then((user) => {
+        expect(user === 'foo').to.equal(true)
+      })
+
+      .get('input[name="http_basic_passwd"]')
+      .invoke('val')
+      .then((pass) => {
+        expect(pass === 'bar').to.equal(true)
       })
   })
-})
 
-describe('Authentication', () => {
-  it(`Change default auth 'None' to 'Basic' and set httpUser and httpPassword with url query`, () => {
-    cy.visit(`?&auth=Basic Auth&httpUser=foo&httpPassword=bar`, { retryOnStatusCodeFailure: true })
-      .get('#auth').contains('Basic Auth').click({ force: true })
-        .then(() => {
-          cy.get('input[name="http_basic_user"]', { timeout: 500 })
-            .invoke('val')
-            .then((user) => {
-              expect(user === 'foo').to.equal(true)
-              cy.log('Success! user === foo')
-            })
-
-          cy.get('input[name="http_basic_passwd"]')
-            .invoke('val')
-            .then((user) => {
-              expect(user === 'bar').to.equal(true)
-              cy.log('Success! password === bar')
-            })
-        })
-  })
-
-  const base64Tkn = encodeURI(btoa('{"alg":"HS256", "typ": "JWT"}'))
-
-  it(`Change default auth 'None' to 'Bearer token' and set bearerToken with url query`, () => {
-    cy.visit(`/?auth=Bearer Token&bearerToken=${base64Tkn}`, { retryOnStatusCodeFailure: true })
-        .get('#auth').contains('Bearer Token').click({ force: true })
-          .then(() => {
-            cy.get('input[name="bearer_token"]', { timeout: 500 })
-              .invoke('val')
-              .then((tkn) => {
-                expect(tkn === base64Tkn).to.equal(true)
-                cy.log(`Success! input[name="bearer_token"] === ${base64Tkn}`)
-              })
-          })
-  })
+  it('Enable user and pass at url with toggler', () => {
+    cy.visit('/', { retryOnStatusCodeFailure: true })
+      .get('#auth')
+      .select('Basic Auth')
+      .get('input[name="http_basic_user"]', { timeout: 500 })
+      .type('foo')
+      .get('input[name="http_basic_passwd"]', { timeout: 500 })
+      .type('bar')
+      .url()
+      .should('not.contain', 'foo')
+      .should('not.contain', 'bar')
+      .get('.toggle')
+      .click()
+      .url()
+      .should('contain', 'foo')
+      .should('contain', 'bar')
+    })
 })
